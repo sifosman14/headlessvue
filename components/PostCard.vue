1aafb998dcc0645d50ebf0791485508c53c97f3e
sifosman <sifosman@gmail.com>
2022-12-26 16:40:00 +0200

    headless Vue Nuxtjs

---
+++ components/PostCard.vue
@@ -0,0 +1,22 @@
+<template>
+<article class="col-12 py-5">
+  <siv class="card shadow">
+    <div class="card-body">
+      <h5 class="card-title">
+        <NuxtLink :to="{ name: 'slug', params: { slug: post.slug } }">{{ post.title }}</NuxtLink>
+      </h5>
+
+      
+      <div class="card-text" v-html="post.excerpt"></div>
+    </div>
+  </siv>
+</article>  
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: ['post'],
+})
+</script>
